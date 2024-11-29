4acfb91b830350c4a551bc0a8cda691746945b3c
Morten Jelle <mortenjelle@Mortens-MacBook-Pro.local>
2024-11-29 14:20:17 +0100

    fix: vite migration

--- apps/react-vite/vite.config.ts
+++ apps/react-vite/vite.config.ts
@@ -1,7 +1,6 @@
 import reactPlugin from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tsconfigPathsPlugin from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [reactPlugin(), tsconfigPathsPlugin()],
+  plugins: [reactPlugin()],
 })
--- apps/solid-js-vite/vite.config.ts
+++ apps/solid-js-vite/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import tsconfigPathsPlugin from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [solidPlugin(), tsconfigPathsPlugin()],
+  plugins: [solidPlugin()],
 })
